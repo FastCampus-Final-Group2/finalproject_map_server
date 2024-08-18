97e3998e875ce3b5cb7e889dee349f8e5c57328e
sinsj5858 <goddnjswns58@gmail.com>
2024-08-18 20:47:09 +0900

    init project

---
+++ src/main/java/com/team2/finalprojectmapserver/FinalprojectMapServerApplication.java
@@ -0,0 +1,13 @@
+package com.team2.finalprojectmapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectMapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectMapServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team2/finalprojectmapserver/FinalprojectMapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.finalprojectmapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectMapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
